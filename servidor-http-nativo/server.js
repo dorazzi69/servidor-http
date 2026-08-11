c346a833ae1478c26e2e3dfaabd48243095cf9ba
dorazzi <dorazzireishenrique@gmail.com>
2026-08-11 20:02:13 +0000

    feat: created server

---
+++ servidor-http-nativo/server.js
@@ -0,0 +1,19 @@
+const http = require('node:http')
+
+const porta = 3000
+
+const server = http.createServer();
+
+server.on('request', (req,res) =>{
+    console.log(`Servidor Funcionando! ${req.method} ${req.url}`);
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.end("Servidor Funcionando!");
+
+
+});
+
+server.listen(porta, () =>{
+    console.log(`Servidor ouvindo na porta ${porta}`)
+});
