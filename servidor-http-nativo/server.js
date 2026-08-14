654fb96928904a0b50286f950e5d7990c576a0a8
dorazzi <dorazzireishenrique@gmail.com>
2026-08-14 18:17:46 +0000

    edit: mudei para o es6

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,8 +1,9 @@
-const http = require('node:http')
+import http from 'node:http'
 
 const porta = 3000
 
 const server = http.createServer();
+
 
 server.on('request', (req,res) =>{
     console.log(`Servidor Funcionando! ${req.method} ${req.url}`);
