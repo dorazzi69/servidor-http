b8b93df9c538d9cbf1caab789f08c02e2ccb1b4c
dorazzi <dorazzireishenrique@gmail.com>
2026-08-14 20:52:01 +0000

    salvando antes de fazer as atividades

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,17 +1,32 @@
 import http from 'node:http'
+import { URL } from 'node:url'
 
 const porta = 3000
 
 const server = http.createServer();
 
-server.on('request', (req, res) => {
-    console.log(`Servidor Funcionando! ${req.method} ${req.url}`);
+const requisicao = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+     const urlObj = new URL(req.url, `http://${req.headers.host}`);
+
+    if (req.method === 'GET' && urlObj.pathname === '/saudacao') {
+        const nome = urlObj.searchParams.get('nome');
+        return res.end(JSON.stringify({"nome": nome}))
+    }
+   
+    const nome = urlObj.searchParams.get('nome');
+    console.log(nome);
+    return res.write(JSON.stringify({ "nome": nome }));
+
+    return res.end(JSON.stringify({ "chave": "valor" }));
 
 
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    //o servidor nao carrega,pois fica aguardando infinitamente a conclusao da resposta
-});
+    console.log(`Requisição recebida! ${req.method} ${req.url}`);
+    res.end();
+}
+
+server.on('request', requisicao);
 
 server.listen(porta, () => {
     console.log(`Servidor ouvindo na porta ${porta}`)
